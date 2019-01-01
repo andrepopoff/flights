40c2bd4f26fda513faa8ac50c26244f6432698aa
andrepopoff <andrepopoff@gmail.com>
2019-01-01 20:54:58 +0400

    Now we can get xml data from files

--- aviatickets/ticketsapi/xml_parser.py
+++ aviatickets/ticketsapi/xml_parser.py
@@ -2,8 +2,16 @@ import json
 from bs4 import BeautifulSoup
 
 
+def get_xml_data(file_name):
+    with open(file_name, 'r') as file:
+        return file.read()
+
+
 def main():
-    pass
+    xml_file_paths = ('xml_files/RS_Via-3.xml', 'xml_files/RS_ViaOW.xml')
+    for file in xml_file_paths:
+        xml_data = get_xml_data(file)
+        print(xml_data)
 
 
 if __name__ == '__main__':
