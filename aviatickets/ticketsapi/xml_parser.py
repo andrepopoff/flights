19879ba9c517f811471e45569b72b74d93dcee67
andrepopoff <andrepopoff@gmail.com>
2019-01-02 01:43:00 +0400

    Created from_xml_to_dict mock-function

--- aviatickets/ticketsapi/xml_parser.py
+++ aviatickets/ticketsapi/xml_parser.py
@@ -7,11 +7,15 @@ def get_xml_data(file_name):
         return file.read()
 
 
+def from_xml_to_dict(xml_data):
+    pass
+
+
 def main():
     xml_file_paths = ('xml_files/RS_Via-3.xml', 'xml_files/RS_ViaOW.xml')
     for file in xml_file_paths:
         xml_data = get_xml_data(file)
-        print(xml_data)
+        data = from_xml_to_dict(xml_data)
 
 
 if __name__ == '__main__':
