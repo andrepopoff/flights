4a0cec38aae751ef8c7d76a0f61c53f966c2ba3f
andrepopoff <andrepopoff@gmail.com>
2019-01-02 17:47:18 +0400

    Added class FlightsView to views.py

--- aviatickets/ticketsapi/views.py
+++ aviatickets/ticketsapi/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from ticketsapi.xml_parser import main
+
+
+class FlightsView(APIView):
+    def get(self, request):
+        result = main('xml_files/RS_Via-3.xml')
+        response = Response(result, status=status.HTTP_200_OK)
+        return response
+    
