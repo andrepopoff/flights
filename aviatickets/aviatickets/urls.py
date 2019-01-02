2636654bc1e1643bfa307a0a5004fca56dcb1dc1
andrepopoff <andrepopoff@gmail.com>
2019-01-02 18:06:44 +0400

    Added url to display data from FlightView

--- aviatickets/aviatickets/urls.py
+++ aviatickets/aviatickets/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from ticketsapi.views import FlightsView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('all/', FlightsView.as_view())
 ]
