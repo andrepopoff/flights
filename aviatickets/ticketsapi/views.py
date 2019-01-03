5bc2a4d854c495a4b24726e13830c4cc6f757d30
andrepopoff <andrepopoff@gmail.com>
2019-01-03 21:22:58 +0400

    Added the ability to add a 'return' parameter to the flights.get request. Depending on the parameter, a different result is displayed.

--- aviatickets/ticketsapi/views.py
+++ aviatickets/ticketsapi/views.py
@@ -8,5 +8,11 @@ from ticketsapi.xml_parser import main
 
 class FlightsView(APIView):
     def get(self, request):
-        result = main('ticketsapi/xml_files/RS_Via-3.xml')
+        return_flights = request.GET.get('return', '1')
+        if return_flights == '0':
+            result = main('ticketsapi/xml_files/RS_ViaOW.xml')
+        elif return_flights == '1':
+            result = main('ticketsapi/xml_files/RS_Via-3.xml')
+        else:
+            return JsonResponse({'error': 'Bad Request (400)'}, status=status.HTTP_400_BAD_REQUEST)
         return JsonResponse({'response': result}, status=status.HTTP_200_OK)
