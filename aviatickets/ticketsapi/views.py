9c24e7b259ad24dc8ef0e6766192c0f432f9ec63
andrepopoff <andrepopoff@gmail.com>
2019-01-03 15:14:29 +0400

    Response --> JsonResponse

--- aviatickets/ticketsapi/views.py
+++ aviatickets/ticketsapi/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response
+# from rest_framework.response import Response
 from rest_framework import status
+from django.http import JsonResponse
 
 from ticketsapi.xml_parser import main
 
@@ -8,5 +9,4 @@ from ticketsapi.xml_parser import main
 class FlightsView(APIView):
     def get(self, request):
         result = main('ticketsapi/xml_files/RS_Via-3.xml')
-        response = Response(result, status=status.HTTP_200_OK)
-        return response
+        return JsonResponse(result, status=status.HTTP_200_OK)
