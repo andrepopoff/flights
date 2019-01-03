553acaebf0863a7da47d1fec225880d19e375772
andrepopoff <andrepopoff@gmail.com>
2019-01-03 13:05:00 +0400

    Slightly changed the names of the keys of the data dictionary

--- aviatickets/ticketsapi/xml_parser.py
+++ aviatickets/ticketsapi/xml_parser.py
@@ -15,18 +15,18 @@ def get_tickets_type(xml_data):
 
 def get_flight_data(flight_tag):
     return {
-        'carrier-id': flight_tag.find('Carrier').get('id'),
-        'carrier-name': flight_tag.find('Carrier').text,
-        'flight-number': flight_tag.find('FlightNumber').text,
+        'carrier_id': flight_tag.find('Carrier').get('id'),
+        'carrier_name': flight_tag.find('Carrier').text,
+        'flight_number': flight_tag.find('FlightNumber').text,
         'source': flight_tag.find('Source').text,
         'destination': flight_tag.find('Destination').text,
-        'departure-time': flight_tag.find('DepartureTimeStamp').text,
-        'arrival-time': flight_tag.find('ArrivalTimeStamp').text,
+        'departure_time': flight_tag.find('DepartureTimeStamp').text,
+        'arrival_time': flight_tag.find('ArrivalTimeStamp').text,
         'class': flight_tag.find('Class').text,
-        'number-of-stops': flight_tag.find('NumberOfStops').text,
-        'fare-basis': flight_tag.find('FareBasis').text.strip(),
-        'warning-text': flight_tag.find('WarningText').text.strip(),
-        'ticket-type': flight_tag.find('TicketType').text
+        'number_of_stops': flight_tag.find('NumberOfStops').text,
+        'fare_basis': flight_tag.find('FareBasis').text.strip(),
+        'warning_text': flight_tag.find('WarningText').text.strip(),
+        'ticket_type': flight_tag.find('TicketType').text
     }
 
 
@@ -39,7 +39,7 @@ def add_service_charges(service_charges_tags, data):
     for i, charge in enumerate(service_charges_tags):
         data.append({})
         data[i]['type'] = charge.get('type')
-        data[i]['charge-type'] = charge.get('ChargeType')
+        data[i]['charge_type'] = charge.get('ChargeType')
         data[i]['price'] = charge.text
 
 
@@ -48,10 +48,10 @@ def from_xml_to_dict(xml_data):
     data = {'response': {'flights': []}}
     response = data['response']
 
-    response['return-tickets'] = get_tickets_type(xml_data)
-    response['request-time'] = soup.find('AirFareSearchResponse').get('RequestTime')
-    response['response-time'] = soup.find('AirFareSearchResponse').get('ResponseTime')
-    response['request-id'] = soup.find('RequestId').text
+    response['return_tickets'] = get_tickets_type(xml_data)
+    response['request_time'] = soup.find('AirFareSearchResponse').get('RequestTime')
+    response['response_time'] = soup.find('AirFareSearchResponse').get('ResponseTime')
+    response['request_id'] = soup.find('RequestId').text
 
     flights_tags = soup.find('PricedItineraries').children
     clean_flights_tags = [tag for tag in flights_tags if tag != '\n']
@@ -60,16 +60,16 @@ def from_xml_to_dict(xml_data):
         response['flights'].append({'onward': []})
         add_flight_data_to_dict(tag, 'OnwardPricedItinerary', response, num, 'onward')
 
-        if response['return-tickets']:
+        if response['return_tickets']:
             response['flights'][num]['return'] = []
             add_flight_data_to_dict(tag, 'ReturnPricedItinerary', response, num, 'return')
 
         response['flights'][num]['pricing'] = {'currency': tag.find('Pricing').get('currency')}
         pricing = response['flights'][num]['pricing']
-        pricing['service-charges'] = []
+        pricing['service_charges'] = []
 
         service_charges = tag.find('Pricing').find_all('ServiceCharges')
-        add_service_charges(service_charges, pricing['service-charges'])
+        add_service_charges(service_charges, pricing['service_charges'])
 
     return data
 
