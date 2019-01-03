27be2c75d1c72a752acb5e8c02f15bcc7a4c085c
andrepopoff <andrepopoff@gmail.com>
2019-01-03 21:38:45 +0400

    Renamed main function --> get_flights

--- aviatickets/ticketsapi/views.py
+++ aviatickets/ticketsapi/views.py
@@ -3,16 +3,16 @@ from rest_framework.views import APIView
 from rest_framework import status
 from django.http import JsonResponse
 
-from ticketsapi.xml_parser import main
+from ticketsapi.xml_parser import get_flights
 
 
 class FlightsView(APIView):
     def get(self, request):
         return_flights = request.GET.get('return', '1')
         if return_flights == '0':
-            result = main('ticketsapi/xml_files/RS_ViaOW.xml')
+            result = get_flights('ticketsapi/xml_files/RS_ViaOW.xml')
         elif return_flights == '1':
-            result = main('ticketsapi/xml_files/RS_Via-3.xml')
+            result = get_flights('ticketsapi/xml_files/RS_Via-3.xml')
         else:
             return JsonResponse({'error': 'Bad Request (400)'}, status=status.HTTP_400_BAD_REQUEST)
         return JsonResponse({'response': result}, status=status.HTTP_200_OK)
