ffe55cbef340b7797573a8191f16b9ae35017fb9
andrepopoff <andrepopoff@gmail.com>
2019-01-03 15:36:46 +0400

    Added key 'response'

--- aviatickets/ticketsapi/views.py
+++ aviatickets/ticketsapi/views.py
@@ -9,4 +9,4 @@ from ticketsapi.xml_parser import main
 class FlightsView(APIView):
     def get(self, request):
         result = main('ticketsapi/xml_files/RS_Via-3.xml')
-        return JsonResponse(result, status=status.HTTP_200_OK)
+        return JsonResponse({'response': result}, status=status.HTTP_200_OK)
