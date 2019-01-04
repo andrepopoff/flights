aa90b0eebeeae46525036e5b04ca6b0be81c72fc
andrepopoff <andrepopoff@gmail.com>
2019-01-04 12:09:14 +0400

    Now we can get the cheapest and most expensive flights.

--- aviatickets/ticketsapi/views.py
+++ aviatickets/ticketsapi/views.py
@@ -3,16 +3,28 @@ from rest_framework.views import APIView
 from rest_framework import status
 from django.http import JsonResponse
 
-from ticketsapi.xml_parser import get_flights
+from ticketsapi.xml_parser import get_flights, get_at_extreme_prices
 
 
 class FlightsView(APIView):
     def get(self, request):
         return_flights = request.GET.get('return', '1')
+        flight_type = request.GET.get('type', 'all')
+        
         if return_flights == '0':
             result = get_flights('ticketsapi/xml_files/RS_ViaOW.xml')
         elif return_flights == '1':
             result = get_flights('ticketsapi/xml_files/RS_Via-3.xml')
         else:
             return JsonResponse({'error': 'Bad Request (400)'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if flight_type == 'all':
+            pass
+        elif flight_type == 'expensive':
+            result = get_at_extreme_prices(result, max)
+        elif flight_type == 'cheap':
+            result = get_at_extreme_prices(result, min)
+        else:
+            return JsonResponse({'error': 'Bad Request (400)'}, status=status.HTTP_400_BAD_REQUEST)
+
         return JsonResponse({'response': result}, status=status.HTTP_200_OK)
