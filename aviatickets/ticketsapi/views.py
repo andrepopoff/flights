41572123ea9b0500b054116176d4994332fe1491
andrepopoff <andrepopoff@gmail.com>
2019-01-06 08:41:05 +0400

    Paths with os.path.join

--- aviatickets/ticketsapi/views.py
+++ aviatickets/ticketsapi/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 # from rest_framework.response import Response
 from rest_framework import status
 from django.http import JsonResponse
+from os.path import join
 
 from ticketsapi.flights_handler import get_flights, get_by, get_optimal, get_difference
 
@@ -11,9 +12,9 @@ class FlightsView(APIView):
         return_flights = request.GET.get('return', '1')
 
         if return_flights == '0':
-            result = get_flights('ticketsapi/xml_files/RS_ViaOW.xml')
+            result = get_flights(join('ticketsapi', 'xml_files', 'RS_ViaOW.xml'))
         elif return_flights == '1':
-            result = get_flights('ticketsapi/xml_files/RS_Via-3.xml')
+            result = get_flights(join('ticketsapi', 'xml_files', 'RS_Via-3.xml'))
         else:
             return JsonResponse({'error': 'Bad Request (400)'}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -35,7 +36,7 @@ class FlightsView(APIView):
 
 class DifferenceView(APIView):
     def get(self, request):
-        response1 = get_flights('ticketsapi/xml_files/RS_ViaOW.xml')
-        response2 = get_flights('ticketsapi/xml_files/RS_Via-3.xml')
+        response1 = get_flights(join('ticketsapi', 'xml_files', 'RS_ViaOW.xml'))
+        response2 = get_flights(join('ticketsapi', 'xml_files', 'RS_Via-3.xml'))
         result = get_difference(response1, response2)
         return JsonResponse({'response': result}, status=status.HTTP_200_OK)
