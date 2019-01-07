cf8ec7aaa8b672cc2777b48f95ec2d52ba3d593c
andrepopoff <andrepopoff@gmail.com>
2019-01-07 18:21:53 +0400

    Deleted classes. Added functions. Used @api_view decorator

--- aviatickets/ticketsapi/views.py
+++ aviatickets/ticketsapi/views.py
@@ -1,4 +1,4 @@
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework import status
 from django.http import JsonResponse
 from os.path import join
@@ -7,34 +7,35 @@ from os.path import join
 from ticketsapi.flights_handler import get_flights, get_by, get_optimal, get_difference
 
 
-class FlightsView(APIView):
-    def get(self, request, url):
-        return_flights = request.GET.get('return', '1')
+@api_view(['GET'])
+def flights_view(request, url):
+    return_flights = request.GET.get('return', '1')
 
-        if return_flights == '0':
-            result = get_flights(join('ticketsapi', 'xml_files', 'RS_ViaOW.xml'))
-        elif return_flights == '1':
-            result = get_flights(join('ticketsapi', 'xml_files', 'RS_Via-3.xml'))
-        else:
-            return JsonResponse({'error': 'Bad Request (400)'}, status=status.HTTP_400_BAD_REQUEST)
+    if return_flights == '0':
+        result = get_flights(join('ticketsapi', 'xml_files', 'RS_ViaOW.xml'))
+    elif return_flights == '1':
+        result = get_flights(join('ticketsapi', 'xml_files', 'RS_Via-3.xml'))
+    else:
+        return JsonResponse({'error': 'Bad Request (400)'}, status=status.HTTP_400_BAD_REQUEST)
 
-        if url == 'getMostExpensive':
-            result = get_by('price', result, max)
-        elif url == 'getCheapest':
-            result = get_by('price', result, min)
-        elif url == 'getLongest':
-            result = get_by('duration', result, max)
-        elif url == 'getFastest':
-            result = get_by('duration', result, min)
-        elif url == 'getOptimal':
-            result = get_optimal(result)
+    if url == 'getMostExpensive':
+        result = get_by('price', result, max)
+    elif url == 'getCheapest':
+        result = get_by('price', result, min)
+    elif url == 'getLongest':
+        result = get_by('duration', result, max)
+    elif url == 'getFastest':
+        result = get_by('duration', result, min)
+    elif url == 'getOptimal':
+        result = get_optimal(result)
 
-        return JsonResponse({'response': result}, status=status.HTTP_200_OK)
+    return JsonResponse({'response': result}, status=status.HTTP_200_OK)
 
 
-class DifferenceView(APIView):
-    def get(self, request):
-        request1 = get_flights(join('ticketsapi', 'xml_files', 'RS_ViaOW.xml'))
-        request2 = get_flights(join('ticketsapi', 'xml_files', 'RS_Via-3.xml'))
-        result = get_difference(request1, request2)
-        return JsonResponse({'response': result}, status=status.HTTP_200_OK)
+@api_view(['GET'])
+def flights_difference_view(request):
+    request1 = get_flights(join('ticketsapi', 'xml_files', 'RS_ViaOW.xml'))
+    request2 = get_flights(join('ticketsapi', 'xml_files', 'RS_Via-3.xml'))
+    result = get_difference(request1, request2)
+    return JsonResponse({'response': result}, status=status.HTTP_200_OK)
+
