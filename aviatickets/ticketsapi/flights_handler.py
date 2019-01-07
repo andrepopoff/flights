59d350f8e70c51e79ecaf0cc30856cb4465824bc
andrepopoff <andrepopoff@gmail.com>
2019-01-07 16:31:35 +0400

    Added @exc_handler decorator to some functions.

--- aviatickets/ticketsapi/flights_handler.py
+++ aviatickets/ticketsapi/flights_handler.py
@@ -46,6 +46,7 @@ def get_tickets_type(xml_data):
         return 0
 
 
+@exc_handler
 def get_flight_data(flight_tag):
     """
     Parsing flight data from xml tags
@@ -53,25 +54,23 @@ def get_flight_data(flight_tag):
     :param flight_tag: <class 'bs4.element.Tag'>
     :return: dictionary with flight data
     """
-    try:
-        return {
-            'carrier_id': flight_tag.find('Carrier').get('id'),
-            'carrier_name': flight_tag.find('Carrier').text,
-            'flight_number': flight_tag.find('FlightNumber').text,
-            'source': flight_tag.find('Source').text,
-            'destination': flight_tag.find('Destination').text,
-            'departure_time': flight_tag.find('DepartureTimeStamp').text,
-            'arrival_time': flight_tag.find('ArrivalTimeStamp').text,
-            'class': flight_tag.find('Class').text,
-            'number_of_stops': flight_tag.find('NumberOfStops').text,
-            'fare_basis': flight_tag.find('FareBasis').text.strip(),
-            'warning_text': flight_tag.find('WarningText').text.strip(),
-            'ticket_type': flight_tag.find('TicketType').text
-        }
-    except AttributeError as error:
-        print('In func {}: {} {}'.format(get_flight_data.__name__, error.__class__, error))
+    return {
+        'carrier_id': flight_tag.find('Carrier').get('id'),
+        'carrier_name': flight_tag.find('Carrier').text,
+        'flight_number': flight_tag.find('FlightNumber').text,
+        'source': flight_tag.find('Source').text,
+        'destination': flight_tag.find('Destination').text,
+        'departure_time': flight_tag.find('DepartureTimeStamp').text,
+        'arrival_time': flight_tag.find('ArrivalTimeStamp').text,
+        'class': flight_tag.find('Class').text,
+        'number_of_stops': flight_tag.find('NumberOfStops').text,
+        'fare_basis': flight_tag.find('FareBasis').text.strip(),
+        'warning_text': flight_tag.find('WarningText').text.strip(),
+        'ticket_type': flight_tag.find('TicketType').text
+    }
 
 
+@exc_handler
 def add_flight_data_to_dict(soup_obj, itinerary_type, data, flight_number, set_key):
     """
     Adds flight data to the dictionary
@@ -82,13 +81,11 @@ def add_flight_data_to_dict(soup_obj, itinerary_type, data, flight_number, set_k
     :param flight_number: <class 'int'> - flight order in data['flights'] list
     :param set_key: <class 'str'> - key to set data['flights'][flight_number][set_key]
     """
-    try:
-        flight_tags = soup_obj.find(itinerary_type).find_all('Flight')
-        [data['flights'][flight_number][set_key].append(get_flight_data(flight_tag)) for flight_tag in flight_tags]
-    except (AttributeError, KeyError, TypeError, IndexError) as error:
-        print('In func {}: {} {}'.format(add_flight_data_to_dict.__name__, error.__class__, error))
+    flight_tags = soup_obj.find(itinerary_type).find_all('Flight')
+    [data['flights'][flight_number][set_key].append(get_flight_data(flight_tag)) for flight_tag in flight_tags]
 
 
+@exc_handler
 def add_service_charges(service_charges_tags, data):
     """
     Adds service charges data to the dictionary
@@ -96,20 +93,17 @@ def add_service_charges(service_charges_tags, data):
     :param service_charges_tags: <class 'bs4.element.ResultSet'>
     :param data: list to add service charges data
     """
-    try:
-        for i, charge in enumerate(service_charges_tags):
-            data.append({})
-            type_ = data[i]['type'] = charge.get('type')
-            charge_type = data[i]['charge_type'] = charge.get('ChargeType')
-            price = data[i]['price'] = charge.text
+    for i, charge in enumerate(service_charges_tags):
+        data.append({})
+        type_ = data[i]['type'] = charge.get('type')
+        charge_type = data[i]['charge_type'] = charge.get('ChargeType')
+        price = data[i]['price'] = charge.text
 
-            if not type_ or not charge_type or not price:
-                raise Exception('One of the parameters was not found. Wrong data in service_charges_tags')
-
-    except (AttributeError, Exception) as error:
-        print('In func {}: {} {}'.format(add_service_charges.__name__, error.__class__, error))
+        if not type_ or not charge_type or not price:
+            raise Exception('One of the parameters was not found. Wrong data in service_charges_tags')
 
 
+@exc_handler
 def from_xml_to_dict(xml_data):
     """
     From XML data to the dictionary
@@ -117,37 +111,33 @@ def from_xml_to_dict(xml_data):
     :param xml_data: string with XML data
     :return: dictionary with flights data
     """
-    try:
-        soup = BeautifulSoup(xml_data, features='xml')
-        data = dict()
+    soup = BeautifulSoup(xml_data, features='xml')
+    data = dict()
 
-        flights = data['flights'] = []
-        data['return_tickets'] = get_tickets_type(xml_data)
-        data['request_time'] = soup.find('AirFareSearchResponse').get('RequestTime')
-        data['response_time'] = soup.find('AirFareSearchResponse').get('ResponseTime')
-        data['request_id'] = soup.find('RequestId').text
+    flights = data['flights'] = []
+    data['return_tickets'] = get_tickets_type(xml_data)
+    data['request_time'] = soup.find('AirFareSearchResponse').get('RequestTime')
+    data['response_time'] = soup.find('AirFareSearchResponse').get('ResponseTime')
+    data['request_id'] = soup.find('RequestId').text
 
-        flights_tags = soup.find('PricedItineraries').children
-        clean_flights_tags = [tag for tag in flights_tags if tag != '\n']
+    flights_tags = soup.find('PricedItineraries').children
+    clean_flights_tags = [tag for tag in flights_tags if tag != '\n']
 
-        for num, tag in enumerate(clean_flights_tags):
-            flights.append({'onward_itinerary': []})
-            add_flight_data_to_dict(tag, 'OnwardPricedItinerary', data, num, 'onward_itinerary')
+    for num, tag in enumerate(clean_flights_tags):
+        flights.append({'onward_itinerary': []})
+        add_flight_data_to_dict(tag, 'OnwardPricedItinerary', data, num, 'onward_itinerary')
 
-            if data['return_tickets']:
-                flights[num]['return_itinerary'] = []
-                add_flight_data_to_dict(tag, 'ReturnPricedItinerary', data, num, 'return_itinerary')
+        if data['return_tickets']:
+            flights[num]['return_itinerary'] = []
+            add_flight_data_to_dict(tag, 'ReturnPricedItinerary', data, num, 'return_itinerary')
 
-            pricing = flights[num]['pricing'] = {'currency': tag.find('Pricing').get('currency')}
-            pricing['service_charges'] = []
+        pricing = flights[num]['pricing'] = {'currency': tag.find('Pricing').get('currency')}
+        pricing['service_charges'] = []
 
-            service_charges = tag.find('Pricing').find_all('ServiceCharges')
-            add_service_charges(service_charges, pricing['service_charges'])
+        service_charges = tag.find('Pricing').find_all('ServiceCharges')
+        add_service_charges(service_charges, pricing['service_charges'])
 
-        return data
-
-    except (TypeError, AttributeError) as error:
-        print('In func {}: {} {}'.format(from_xml_to_dict.__name__, error.__class__, error))
+    return data
 
 
 def get_flights(xml_file_path):
@@ -161,6 +151,7 @@ def get_flights(xml_file_path):
     return from_xml_to_dict(xml_data)
 
 
+@exc_handler
 def get_total_amounts(flights):
     """
     Calculates the total amount of each flight
@@ -168,21 +159,18 @@ def get_total_amounts(flights):
     :param flights: dictionary with 'flights' key that contain flight data
     :return: total amounts list that contain total amounts for all flights
     """
-    try:
-        total_amounts = []
-        for flight in flights['flights']:
-            amount = 0
-            for charge in flight['pricing']['service_charges']:
-                if charge['charge_type'] == 'TotalAmount':
-                    amount += float(charge['price'])
-            total_amounts.append(amount)
+    total_amounts = []
+    for flight in flights['flights']:
+        amount = 0
+        for charge in flight['pricing']['service_charges']:
+            if charge['charge_type'] == 'TotalAmount':
+                amount += float(charge['price'])
+        total_amounts.append(amount)
 
-        return total_amounts
-
-    except (TypeError, KeyError, ValueError) as error:
-        print('In func {}: {} {}'.format(get_total_amounts.__name__, error.__class__, error))
+    return total_amounts
 
 
+@exc_handler
 def calculate_flight_duration(flight_data):
     """
     Calculates the flight duration
@@ -190,14 +178,12 @@ def calculate_flight_duration(flight_data):
     :param flight_data: dictionary that contain flight data
     :return: <class 'datetime.timedelta'>
     """
-    try:
-        departure_time = datetime.strptime(flight_data[0]['departure_time'], '%Y-%m-%dT%H%M')
-        arrival_time = datetime.strptime(flight_data[-1]['arrival_time'], '%Y-%m-%dT%H%M')
-        return arrival_time - departure_time
-    except (KeyError, IndexError, TypeError, ValueError) as error:
-        print('In func {}: {} {}'.format(calculate_flight_duration.__name__, error.__class__, error))
+    departure_time = datetime.strptime(flight_data[0]['departure_time'], '%Y-%m-%dT%H%M')
+    arrival_time = datetime.strptime(flight_data[-1]['arrival_time'], '%Y-%m-%dT%H%M')
+    return arrival_time - departure_time
 
 
+@exc_handler
 def get_durations(flights):
     """
     Calculates the duration of each flight
@@ -205,21 +191,18 @@ def get_durations(flights):
     :param flights: dictionary with 'flights' key that contain flight data
     :return: durations list that contain durations for all flights
     """
-    try:
-        durations = []
-        for flight in flights['flights']:
-            duration = calculate_flight_duration(flight['onward_itinerary'])
-            if flights['return_tickets']:
-                return_duration = calculate_flight_duration(flight['return_itinerary'])
-                duration = duration + return_duration
-            durations.append(duration)
+    durations = []
+    for flight in flights['flights']:
+        duration = calculate_flight_duration(flight['onward_itinerary'])
+        if flights['return_tickets']:
+            return_duration = calculate_flight_duration(flight['return_itinerary'])
+            duration = duration + return_duration
+        durations.append(duration)
 
-        return durations
-
-    except (TypeError, KeyError) as error:
-        print('In func {}: {} {}'.format(get_durations.__name__, error.__class__, error))
+    return durations
 
 
+@exc_handler
 def get_by(key, flights, func):
     """
     Returns the most expensive/cheapest, longest/fastest flights
@@ -233,18 +216,16 @@ def get_by(key, flights, func):
     If you need to find the longest or most expensive --> max
     :return: dictionary with flights data
     """
-    try:
-        if func not in (max, min):
-            raise TypeError('Parameter func must be only min or max builtin func')
+    if func not in (max, min):
+        raise TypeError('Parameter func must be only min or max builtin func')
 
-        handlers = {'duration': get_durations, 'price': get_total_amounts}
-        all_values = handlers[key](flights)
-        flights['flights'] = [flights['flights'][idx] for idx, val in enumerate(all_values) if val == func(all_values)]
-        return flights
-    except (KeyError, TypeError) as error:
-        print('In func {}: {} {}'.format(get_by.__name__, error.__class__, error))
+    handlers = {'duration': get_durations, 'price': get_total_amounts}
+    all_values = handlers[key](flights)
+    flights['flights'] = [flights['flights'][idx] for idx, val in enumerate(all_values) if val == func(all_values)]
+    return flights
 
 
+@exc_handler
 def get_optimal(flights):
     """
     Finds the best flight option.
@@ -255,17 +236,15 @@ def get_optimal(flights):
     :param flights: dictionary with 'flights' key that contain flight data
     :return: dictionary with optimal flights
     """
-    try:
-        durations = get_durations(flights)
-        average_time = sum([duration.total_seconds() for duration in durations]) / len(durations)
-        flights['flights'] = [
-            flight for idx, flight in enumerate(flights['flights']) if durations[idx].total_seconds() <= average_time
-        ]
-        return get_by('price', flights, min)
-    except (KeyError, TypeError) as error:
-        print('In func {}: {} {}'.format(get_optimal.__name__, error.__class__, error))
+    durations = get_durations(flights)
+    average_time = sum([duration.total_seconds() for duration in durations]) / len(durations)
+    flights['flights'] = [
+        flight for idx, flight in enumerate(flights['flights']) if durations[idx].total_seconds() <= average_time
+    ]
+    return get_by('price', flights, min)
 
 
+@exc_handler
 def check_and_set_params(source1, source2, dict_to_set, set_key):
     """
     Checks the equality of source1 and source2 and adds a key and value to the dictionary
@@ -275,14 +254,12 @@ def check_and_set_params(source1, source2, dict_to_set, set_key):
     :param dict_to_set: dictionary to set data
     :param set_key: dictionary key
     """
-    try:
-        if source1 != source2:
-            dict_to_set['first'][str(set_key)] = source1
-            dict_to_set['second'][str(set_key)] = source2
-    except (KeyError, TypeError) as error:
-        print('In func {}: {} {}'.format(check_and_set_params.__name__, error.__class__, error))
+    if source1 != source2:
+        dict_to_set['first'][str(set_key)] = source1
+        dict_to_set['second'][str(set_key)] = source2
 
 
+@exc_handler
 def get_service_charges_types(service_charges):
     """
     Returns service charge types
@@ -290,12 +267,10 @@ def get_service_charges_types(service_charges):
     :param service_charges: list with dictionaries as objects that contain service charge data
     :return: <class 'set'> with service charge types for flight
     """
-    try:
-        return set(data['type'] for data in service_charges)
-    except (KeyError, TypeError) as error:
-        print('In func {}: {} {}'.format(get_service_charges_types.__name__, error.__class__, error))
+    return set(data['type'] for data in service_charges)
 
 
+@exc_handler
 def get_difference(flights_data1, flights_data2):
     """
     Returns the difference between two flight data dictionaries.
@@ -336,4 +311,4 @@ def get_difference(flights_data1, flights_data2):
 if __name__ == '__main__':
     soup = BeautifulSoup('<return><Flight>air</Flight></return>', features='xml')
     data = {'flights': []}
-    print(get_xml_data([]))
+    print(add_service_charges(soup, []))
