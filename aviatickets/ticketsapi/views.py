b3d07f5cbead699dcc92b3c32baef429b2d658a8
andrepopoff <andrepopoff@gmail.com>
2019-01-15 14:03:35 +0400

    Added docstrings to functions (views.py)

--- aviatickets/ticketsapi/views.py
+++ aviatickets/ticketsapi/views.py
@@ -12,11 +12,23 @@ from ticketsapi.serializers import MethodSerializer
 
 @api_view(['GET'])
 def flights_view(request, url):
+    """
+    View flights.
+
+    * If url = 'getAll', then returns all flights.
+    * If url = 'getMostExpensive', then returns most expensive flights.
+    * If url = 'getCheapest', then returns cheapest flights.
+    * If url = 'getLongest', then returns longest flights.
+    * If url = 'getFastest', then returns fastest flights.
+    * If url = 'getOptimal', then returns the best flights.
+    """
     return_flights = request.GET.get('return', '1')
 
     if return_flights == '0':
+        # Flights without return itineraries
         result = get_flights(join('ticketsapi', 'xml_files', 'RS_ViaOW.xml'))
     elif return_flights == '1':
+        # Flights with return itineraries
         result = get_flights(join('ticketsapi', 'xml_files', 'RS_Via-3.xml'))
     else:
         return JsonResponse({'error': 'Bad Request (400)'}, status=status.HTTP_400_BAD_REQUEST)
@@ -37,6 +49,9 @@ def flights_view(request, url):
 
 @api_view(['GET'])
 def flights_difference_view(request):
+    """
+    View the difference between the two flight requests.
+    """
     request1 = get_flights(join('ticketsapi', 'xml_files', 'RS_ViaOW.xml'))
     request2 = get_flights(join('ticketsapi', 'xml_files', 'RS_Via-3.xml'))
     result = get_difference(request1, request2)
@@ -45,6 +60,10 @@ def flights_difference_view(request):
 
 @api_view(['GET', 'POST'])
 def methods_list(request):
+    """
+    View urls of all methods.
+    API endpoint allows you to use a POST request to add a new url.
+    """
     if request.method == 'GET':
         methods = Method.objects.all()
         serializer = MethodSerializer(methods, many=True)
